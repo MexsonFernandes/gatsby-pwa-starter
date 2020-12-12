b59f27f295142427234dad6417b04cc901523ce6
Mexson Fernandes <robomex2020@gmail.com>
2020-12-12 22:58:00 +0530

    :frog: create starter with service update

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,22 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
+export const onServiceWorkerUpdateReady = async args => {
+  // eslint-disable-next-line no-undef
+  const permissionResponse = await Notification.requestPermission()
+  if (permissionResponse === "granted") {
+    await args.serviceWorker.showNotification("Website update", {
+      body:
+        "Our website just got a little bit better. We reloaded the site with the update to ensure a smooth experience for you.",
+    })
+  }
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+  if (answer === true) {
+    window.location.reload()
+  }
+}
 
-// You can delete this file if you're not using it
+export const registerServiceWorker = () => true
